9de0a10023fe5c157243fbd69b16986fb7ecba73
Sébastien Campion <sebastien.campion@gmail.com>
2025-01-15 09:40:33 +0100

    refactor: Simplify response body handling in body_filter.lua

--- lua-scripts/body_filter.lua
+++ lua-scripts/body_filter.lua
@@ -1,18 +1,14 @@
+if ngx.status ~= 200 then
+    ngx.log(ngx.ERR, "Request failed ", ngx.status)
+    -- forward the error to the client
+    return
+end
+
 -- Capture response body for logging
 local resp_body = ngx.arg[1]
 ngx.ctx.buffered = (ngx.ctx.buffered or "") .. resp_body
 
 if ngx.arg[2] then
     local response_body = ngx.ctx.buffered
-    -- Check if response is JSON
-    local content_type = ngx.header.content_type
-    if content_type and string.find(content_type:lower(), "application/json") then
-        -- Set the nginx variable for logging
-        ngx.var.resp_body = response_body
-        -- Also log to error log for debugging
-        ngx.log(ngx.INFO, response_body)
-    else
-        -- Clear the response body for non-JSON content
-        ngx.var.resp_body = ''
-    end
+    ngx.var.resp_body = response_body
 end
