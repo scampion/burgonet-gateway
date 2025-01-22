ae81058395cfe03960d8838f3856c5beba0f62b1
Sébastien Campion <sebastien.campion@gmail.com>
2025-01-22 14:01:06 +0100

    chore: Add license headers to src/parsers.rs and src/pii_protection.rs

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 Sébastien Campion, FOSS4. All rights reserved.
+//
+// This software is provided under the Commons Clause License Condition v1.0.
+// See the LICENSE file for full license details.
+
 use serde_json::Value;
 use anyhow::{Result, anyhow};
 
--- src/pii_protection.rs
+++ src/pii_protection.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 Sébastien Campion, FOSS4. All rights reserved.
+//
+// This software is provided under the Commons Clause License Condition v1.0.
+// See the LICENSE file for full license details.
+
 use anyhow::Result;
 use base64::engine::general_purpose;
 use base64::Engine;
