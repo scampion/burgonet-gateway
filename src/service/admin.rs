21d5b406b35c0277d0bce3c055a7fe57e7605f59
Sébastien Campion <sebastien.campion@gmail.com>
2025-01-22 14:01:26 +0100

    chore: Add license header to src/service/admin.rs

--- src/service/admin.rs
+++ src/service/admin.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 Sébastien Campion, FOSS4. All rights reserved.
+//
+// This software is provided under the Commons Clause License Condition v1.0.
+// See the LICENSE file for full license details.
+
 use crate::app::admin::HttpAdminApp;
 use pingora::services::listening::Service;
 use std::sync::Arc;
